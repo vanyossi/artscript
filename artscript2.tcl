ec50bbf25cd88af31697aba622847613dc38a7ab
Ivan Yossi <ghevan@gmail.com>
2013-10-09 23:04:04 -0500

    FIX: Hide dot files in linux

--- artscript2.tcl
+++ artscript2.tcl
@@ -16,6 +16,10 @@
 #   File must be in the same directory as the script.
 #
 package require Tk
+# Do not show .dot files by default
+catch {tk_getOpenFile foo bar}
+set ::tk::dialog::file::showHiddenVar 0
+set ::tk::dialog::file::showHiddenBtn 1
 ttk::style theme use clam
 namespace eval ::img { }
 
